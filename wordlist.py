f7dd1420ad6bfb80e49779e64838c627528b5b61
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-07-22 09:29:09 +0100

    create wordlist perssonel

---
+++ wordlist.py
@@ -0,0 +1,267 @@
+#!/usr/bin/python
+# coding=<encoding name>
+import os, sys
+a = ["p","h","g","s","q"]
+
+ze = input( "enter name :")
+zo = input("enter nechname :")
+zi = input("enter nme  :")
+zu = input("enter child :")
+za = input("enter ? :")
+
+a[0]= ze
+a[1]= zo
+a[2]= zi
+a[3]= zu
+a[4]= za
+s = ["*","$","%","/","@","+","&","_","!","?","#"]
+b = 0
+tk = 0
+for i in a :
+     for x in s:
+         b = b + 1
+         print(a[0] + x)
+         print(a[1] + x)
+         print(a[2] + x)
+         print(a[3] + x)
+         print(a[4] + x)
+         print(a[0] + x + s[1])
+         print(a[1] + x + s[2])
+         print(a[2] + x + s[3])
+         print(a[3] + x + s[4])
+         print(a[4] + x + s[5])
+         print(a[0] + x + s[6])
+         print(a[1] + x + s[7])
+         print(a[2] + x + s[8])
+         print(a[3] + x + s[9])
+         print(a[4] + x + s[10])
+         print(a[4] + x)
+         print(a[0] + x + s[1]+s[10])
+         print(a[1] + x + s[2]+s[9])
+         print(a[2] + x + s[3]+s[8])
+         print(a[3] + x + s[4]+s[7])
+         print(a[4] + x + s[5]+s[6])
+         print(a[0] + x + s[6]+s[5])
+         print(a[1] + x + s[7]+s[4])
+         print(a[2] + x + s[8]+s[3])
+         print(a[3] + x + s[9]+s[2])
+         print(a[4] + x + s[10]+s[1])
+         
+         print(a[0] + x + s[1]+s[10]+x)
+         print(a[1] + x + s[2]+s[9]+x)
+         print(a[2] + x + s[3]+s[8]+x)
+         print(a[3] + x + s[4]+s[7]+x)
+         print(a[4] + x + s[5]+s[6]+x)
+         print(a[0] + x + s[6]+s[5]+x)
+         print(a[1] + x + s[7]+s[4]+x)
+         print(a[2] + x + s[8]+s[3]+x)
+         print(a[3] + x + s[9]+s[2]+x)
+         print(a[4] + x + s[10]+s[1]+x)
+          
+               
+         print(a[1] + str(b)+s[0])
+         print(a[1] + str(b)+s[1])
+         print(a[1] + str(b)+s[2])
+         print(a[1] + str(b)+s[3])
+         print(a[1] + str(b)+s[4])
+         print(a[1] + str(b)+s[5])
+         print(a[1] + str(b)+s[6])
+         print(a[1] + str(b)+s[7])
+         print(a[1] + str(b)+s[8])
+         print(a[1] + str(b)+s[9])
+         print(a[1] + str(b)+s[10])
+                  
+
+         print(a[2] + str(b)+s[0])
+         print(a[2] + str(b)+s[1])
+         print(a[2] + str(b)+s[2])
+         print(a[2] + str(b)+s[3])
+         print(a[2] + str(b)+s[4])
+         print(a[2] + str(b)+s[5])
+         print(a[2] + str(b)+s[6])
+         print(a[2] + str(b)+s[7])
+         print(a[2] + str(b)+s[8])
+         print(a[2] + str(b)+s[9])
+         print(a[2] + str(b)+s[10])
+                 
+         print(a[3] + str(b)+s[0])
+         print(a[3] + str(b)+s[1])
+         print(a[3] + str(b)+s[2])
+         print(a[3] + str(b)+s[3])
+         print(a[3] + str(b)+s[4])
+         print(a[3] + str(b)+s[5])
+         print(a[3] + str(b)+s[6])
+         print(a[3] + str(b)+s[7])
+         print(a[3] + str(b)+s[8])
+         print(a[3] + str(b)+s[9])
+         print(a[3] + str(b)+s[10])
+
+
+         print(a[4] + str(b)+s[0])
+         print(a[4] + str(b)+s[1])
+         print(a[4] + str(b)+s[2])
+         print(a[4] + str(b)+s[3])
+         print(a[4] + str(b)+s[4])
+         print(a[4] + str(b)+s[5])
+         print(a[4] + str(b)+s[6])
+         print(a[4] + str(b)+s[7])
+         print(a[4] + str(b)+s[8])
+         print(a[4] + str(b)+s[9])
+         print(a[4] + str(b)+s[10])
+         while b < 1234 :
+              b = b +1
+              print(a[0] + str(b))
+              print(a[1] + str(b))
+              print(a[2] + str(b))
+              print(a[3] + str(b))
+              print(a[4] + str(b))
+
+              while tk < 1234 :
+                  tk = tk +1
+                  print(str(tk) + a[0])
+                  print(str(tk) + a[1])
+                  print(str(tk) + a[2])
+                  print(str(tk) + a[3])
+                  print(str(tk) + a[4])
+
+a[0]= ze.upper()
+a[1]= zo.upper()
+a[2]= zi.upper()
+a[3]= zu.upper()
+a[4]= za.upper()
+s = ["*","$","%","/","@","+","&","_","!","?","#"]
+b = 0
+
+for i in a :
+     for x in s:
+         b = b + 1
+         print(a[0] + x)
+         print(a[1] + x)
+         print(a[2] + x)
+         print(a[3] + x)
+         print(a[4] + x)
+         print(a[0] + x + s[1])
+         print(a[1] + x + s[2])
+         print(a[2] + x + s[3])
+         print(a[3] + x + s[4])
+         print(a[4] + x + s[5])
+         print(a[0] + x + s[6])
+         print(a[1] + x + s[7])
+         print(a[2] + x + s[8])
+         print(a[3] + x + s[9])
+         print(a[4] + x + s[10])
+         print(a[4] + x)
+         print(a[0] + x + s[1]+s[10])
+         print(a[1] + x + s[2]+s[9])
+         print(a[2] + x + s[3]+s[8])
+         print(a[3] + x + s[4]+s[7])
+         print(a[4] + x + s[5]+s[6])
+         print(a[0] + x + s[6]+s[5])
+         print(a[1] + x + s[7]+s[4])
+         print(a[2] + x + s[8]+s[3])
+         print(a[3] + x + s[9]+s[2])
+         print(a[4] + x + s[10]+s[1])
+         
+         print(a[0] + x + s[1]+s[10]+x)
+         print(a[1] + x + s[2]+s[9]+x)
+         print(a[2] + x + s[3]+s[8]+x)
+         print(a[3] + x + s[4]+s[7]+x)
+         print(a[4] + x + s[5]+s[6]+x)
+         print(a[0] + x + s[6]+s[5]+x)
+         print(a[1] + x + s[7]+s[4]+x)
+         print(a[2] + x + s[8]+s[3]+x)
+         print(a[3] + x + s[9]+s[2]+x)
+         print(a[4] + x + s[10]+s[1]+x)
+          
+               
+         print(a[1] + str(b)+s[0])
+         print(a[1] + str(b)+s[1])
+         print(a[1] + str(b)+s[2])
+         print(a[1] + str(b)+s[3])
+         print(a[1] + str(b)+s[4])
+         print(a[1] + str(b)+s[5])
+         print(a[1] + str(b)+s[6])
+         print(a[1] + str(b)+s[7])
+         print(a[1] + str(b)+s[8])
+         print(a[1] + str(b)+s[9])
+         print(a[1] + str(b)+s[10])
+                  
+
+         print(a[2] + str(b)+s[0])
+         print(a[2] + str(b)+s[1])
+         print(a[2] + str(b)+s[2])
+         print(a[2] + str(b)+s[3])
+         print(a[2] + str(b)+s[4])
+         print(a[2] + str(b)+s[5])
+         print(a[2] + str(b)+s[6])
+         print(a[2] + str(b)+s[7])
+         print(a[2] + str(b)+s[8])
+         print(a[2] + str(b)+s[9])
+         print(a[2] + str(b)+s[10])
+                 
+         print(a[3] + str(b)+s[0])
+         print(a[3] + str(b)+s[1])
+         print(a[3] + str(b)+s[2])
+         print(a[3] + str(b)+s[3])
+         print(a[3] + str(b)+s[4])
+         print(a[3] + str(b)+s[5])
+         print(a[3] + str(b)+s[6])
+         print(a[3] + str(b)+s[7])
+         print(a[3] + str(b)+s[8])
+         print(a[3] + str(b)+s[9])
+         print(a[3] + str(b)+s[10])
+
+
+         print(a[4] + str(b)+s[0])
+         print(a[4] + str(b)+s[1])
+         print(a[4] + str(b)+s[2])
+         print(a[4] + str(b)+s[3])
+         print(a[4] + str(b)+s[4])
+         print(a[4] + str(b)+s[5])
+         print(a[4] + str(b)+s[6])
+         print(a[4] + str(b)+s[7])
+         print(a[4] + str(b)+s[8])
+         print(a[4] + str(b)+s[9])
+         print(a[4] + str(b)+s[10])
+         while b < 1234 :
+              b = b +1
+              print(a[0] + str(b))
+              print(a[1] + str(b))
+              print(a[2] + str(b))
+              print(a[3] + str(b))
+              print(a[4] + str(b))   
+
+bb = 0 
+ss = 0
+t = ['*','%','$','&','@','§','?','!','-','_','+']
+
+for i in t :
+
+
+   print(a[0]+ str(i))
+   print(a[1]+ str(i*2))
+   print(a[2]+ str(i*3))
+   print(a[3]+ str(i*4))
+   print(a[4]+ str(i*5))
+   print(a[0]+ str(i*6))
+   print(a[1]+ str(i*7))
+   print(a[2]+ str(i*8))
+   print(a[3]+ str(i*9))
+   print(a[4]+ str(i*10))
+
+
+   while ss < 1234 :
+       ss = ss +1
+       print(a[0] + str(ss))
+       print(a[1] + str(ss))
+       print(a[2] + str(ss))
+       print(a[3] + str(ss))
+       print(a[4] + str(ss))
+       while bb < 1234 :
+           bb = bb +1
+           print(str(bb) + a[0])
+           print(str(bb) + a[1])
+           print(str(bb) + a[2])
+           print(str(bb) + a[3])
+           print(str(bb) + a[4])
+                   
