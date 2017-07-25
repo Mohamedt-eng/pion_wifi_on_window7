ed85d37e49ac7a6abd52127131fbd8a1198ce4f6
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-07-25 07:48:28 -0400

    start-wifi-on windows.bat

---
+++ start-wifi.bat
@@ -0,0 +1,69 @@
+@echo off
+@break off
+title gholam
+color 3E
+echo ouvrir le programe  wifinet avec admisratur qlik adroit maouss; merci>wifi-.txt  
+
+echo  X   
+echo  X
+:0
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                               
+:1
+echo  X      analyser wifi qlik sur___________(1)       X     
+:2
+echo  X      demarre wifi qlik sur____________(2)       X
+:3
+echo  X      afeche information de reseu______(3)       X          
+:4
+echo  X      stop information de reseu _______(4)       X   
+:5
+echo  X      stop wifi qlik sur ______________(5)       X
+
+echo  X                                                 X
+echo  X                                                 X
+echo  X  attent la line (6) ouvrir le program on admini X
+echo  X  attent la line (10) arétte ordinateur          X 
+echo  X__________________________________________       X
+echo  X                                                 X
+:6
+echo  X      defragmente disk C: qlik sur-----(6)       X
+:10     
+echo  X      start traduction qlik sur--------(7)       X
+:8
+echo  X      start google qlik sur------------(8)       X
+:9
+echo  X      start youtube qlik sur-----------(9)       X
+:7
+echo  X      arrette ordinateur qlik sur-----(10)       X
+:00
+echo  X     netwayé afichage qlik sur________(00)       X
+:11
+echo  X     sortir session qlik sur__________(11)       X
+echo  X                                                 X
+echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo  XX
+                                               
+echo  XX GHOLAM                    
+echo  XX                \       /
+echo  XX                 \     /
+echo  XX               ___\___/__
+echo  XX              [# # # #°°°]
+echo  XX              [==========] 
+echo  xx                 router
+echo  XX-------------------------------
+set/P input= ENTRE NEMBER-
+
+if %input%==1 netsh wlan set hostednetwork mod=allow ssid=wifi-soft key=12345678
+if %input%==2 netsh wlan start hostednetwork  
+if %input%==3 netsh wlan set allowexplicitcreds allow=yes
+if %input%==4 netsh wlan set allowexplicitcreds allow=no
+if %input%==5 netsh wlan stop hostednetwork
+if %input%==6 defrag c:
+if %input%==7 start http://www.translate.google.com/?hl=fr
+if %input%==8 start http://www.google.com/
+if %input%==9 start http://www.youtube.com/
+if %input%==10 shutdown -p
+if %input%==00 cls
+if %input%==11 exit
+goto :0
+PAUSE
