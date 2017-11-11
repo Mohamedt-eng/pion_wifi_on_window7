dd821f89f1b2ce88d33f6223cb446d5744639c1f
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-11-11 16:14:24 -0500

    Update start-wifi.bat

--- start-wifi.bat
+++ start-wifi.bat
@@ -9,15 +9,15 @@ echo  X
 :0
 echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX                               
 :1
-echo  X      analyser wifi qlik sur___________(1)       X     
+echo  X      analyser wifi click on___________(1)       X     
 :2
-echo  X      demarre wifi qlik sur____________(2)       X
+echo  X      demarre wifi click on____________(2)       X
 :3
 echo  X      afeche information de reseu______(3)       X          
 :4
 echo  X      stop information de reseu _______(4)       X   
 :5
-echo  X      stop wifi qlik sur ______________(5)       X
+echo  X      stop wifi click on ______________(5)       X
 
 echo  X                                                 X
 echo  X                                                 X
@@ -26,19 +26,19 @@ echo  X  attent la line (10) arétte ordinateur          X
 echo  X__________________________________________       X
 echo  X                                                 X
 :6
-echo  X      defragmente disk C: qlik sur-----(6)       X
+echo  X      defragmente disk C: click on-----(6)       X
 :10     
-echo  X      start traduction qlik sur--------(7)       X
+echo  X      start traduction click on--------(7)       X
 :8
-echo  X      start google qlik sur------------(8)       X
+echo  X      start google click on------------(8)       X
 :9
-echo  X      start youtube qlik sur-----------(9)       X
+echo  X      start youtube click on-----------(9)       X
 :7
-echo  X      arrette ordinateur qlik sur-----(10)       X
+echo  X      arrette ordinateur click on-----(10)       X
 :00
-echo  X     netwayé afichage qlik sur________(00)       X
+echo  X     netwayé afichage click on________(00)       X
 :11
-echo  X     sortir session qlik sur__________(11)       X
+echo  X     exit   session click on__________(11)       X
 echo  X                                                 X
 echo  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 echo  XX
